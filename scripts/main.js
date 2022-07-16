94ef871815728930f890801609b4d15fb0cf5093
Deivid <dejesusdemouradeivid@gmail.com>
2022-07-16 00:57:36 -0300

    update

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var home = document.querySelector('.sec');
+
+
+function show() {
+    if(home.style.display === 'block') {
+      home.style.display = 'none';
+  } else {
+      home.style.display = 'block';
+  } }
