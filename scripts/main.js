d0f37c53c0709dc985311ec695d5704de0f0312c
Deivid <dejesusdemouradeivid@gmail.com>
2022-07-16 22:33:13 -0300

    up

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,18 @@
-var home = document.querySelector('.sec');
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const section = document.getElementById('section');
 
 
-function show() {
+function h() {
     if(home.style.display === 'block') {
       home.style.display = 'none';
   } else {
       home.style.display = 'block';
+  } }
+
+function a() {
+    if(about.style.display === 'block') {
+      about.style.display = 'none';
+  } else {
+      about.style.display = 'block';
   } }
