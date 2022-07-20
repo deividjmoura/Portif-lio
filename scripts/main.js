ca2fbf9c847106d8e9b47e5bcc0418380a234abd
Deivid <dejesusdemouradeivid@gmail.com>
2022-07-20 00:03:22 -0300

    up

--- scripts/main.js
+++ scripts/main.js
@@ -1,56 +1,69 @@
-const home = document.getElementById('home');
+const init = document.getElementById('init');
+// const home = document.getElementById('home');
 const about = document.getElementById('about');
 const certifications = document.getElementById('certifications');
 const contact = document.getElementById('contact');
 
+/*
 function h() {
-    if(home.style.display === 'none' || contact.style.display === 'block' || certifications.style.display === 'block' || about.style.display === 'block') {
+    if(home.style.display === 'none' || init.style.display === 'block' || contact.style.display === 'block' || certifications.style.display === 'block' || about.style.display === 'block') {
         home.style.display = 'block';
+        init.style.display = 'none'
         contact.style.display = 'none' 
         certifications.style.display = 'none'
         about.style.display = 'none'
-        } else if (about.style.display === 'block' || contact.style.display === 'none' || certifications.style.display === 'none' || about.style.display === 'none') {
+        } else if (init.style.display === 'none' || about.style.display === 'block' || contact.style.display === 'none' || certifications.style.display === 'none' || about.style.display === 'none') {
         home.style.display = 'none'
+        init.style.display = 'block' 
   }
     else {
         home.style.display = 'block'
+        init.style.display = 'none'
     }}
+*/
 
 function a() {
-    if(about.style.display === 'none' || contact.style.display === 'block' || certifications.style.display === 'block' || home.style.display === 'block') {
+    if(about.style.display === 'none' || init.style.display === 'block' || contact.style.display === 'block' || certifications.style.display === 'block') {
         about.style.display = 'block';
+        init.style.display = 'none'
         contact.style.display = 'none' 
         certifications.style.display = 'none'
-        home.style.display = 'none'
-        } else if (about.style.display === 'block' || contact.style.display === 'none' || certifications.style.display === 'none' || home.style.display === 'none') {
+        } else if (init.style.display === 'none' || about.style.display === 'block' || contact.style.display === 'none' || certifications.style.display === 'none') {
         about.style.display = 'none'
+        init.style.display = 'block' 
   }
     else {
         about.style.display = 'block'
+        init.style.display = 'none'
     }}
 
-    function c() {
-        if(certifications.style.display === 'none' || contact.style.display === 'block' || about.style.display === 'block' || home.style.display === 'block') {
+function c() {
+    if(certifications.style.display === 'none' || init.style.display === 'block' || contact.style.display === 'block' || about.style.display === 'block') {
+        certifications.style.display = 'block'
+        init.style.display = 'none'
+        about.style.display = 'none';
+        contact.style.display = 'none' 
+        } else if (init.style.display === 'none' || certifications.style.display === 'block' || contact.style.display === 'none' || about.style.display === 'none') {
+        certifications.style.display = 'none'
+        init.style.display = 'block' 
+  }
+    else {
             certifications.style.display = 'block'
-            about.style.display = 'none';
-            contact.style.display = 'none' 
-            home.style.display = 'none'
-            } else if (certifications.style.display === 'block' || contact.style.display === 'none' || about.style.display === 'none' || home.style.display === 'none') {
-            certifications.style.display = 'none'
-      }
-        else {
-            certifications.style.display = 'block'
-        }}
+            init.style.display = 'none'
+    }}
 
-        function ctt() {
-            if(contact.style.display === 'none' || certifications.style.display === 'block' || about.style.display === 'block' || home.style.display === 'block') {
-                contact.style.display = 'block'
-                about.style.display = 'none';
-                certifications.style.display = 'none' 
-                home.style.display = 'none'
-                } else if (contact.style.display === 'block' || certifications.style.display === 'none' || about.style.display === 'none' || home.style.display === 'none') {
-                contact.style.display = 'none'
-          }
-            else {
-                contact.style.display = 'block'
-            }}
+function ctt() {
+    if(contact.style.display === 'none' || init.style.display === 'block' || certifications.style.display === 'block' || about.style.display === 'block') {
+        contact.style.display = 'block'
+        init.style.display = 'none'
+        about.style.display = 'none';
+        certifications.style.display = 'none' 
+        } else if (init.style.display === 'none' || contact.style.display === 'block' || certifications.style.display === 'none' || about.style.display === 'none') {
+        contact.style.display = 'none'
+        init.style.display = 'block' 
+        }
+    else {
+        contact.style.display = 'block'
+        init.style.display = 'none'
+    }}
+
