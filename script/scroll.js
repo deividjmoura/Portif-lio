669adb875f5c6ed369ac707f5080a974d1c05085
Deivid De Jesus De Moura <86139999+deividjmoura@users.noreply.github.com>
2024-04-18 17:59:27 +0000

    more addccion

--- script/scroll.js
+++ script/scroll.js
@@ -4,8 +4,8 @@ $(function() {
         setTimeout(function() {
             $('html, body').stop().animate({
                 scrollTop: $($anchor.attr('href')).offset().top
-            }, 5000, 'easeInOutExpo'); // Alterei o tempo de animação para 5000ms (5 segundos)
-        }, 500); // Delay de 500ms antes de iniciar o scroll
+            }, 500, 'easeInOutExpo'); // Alterei o tempo de animação para 5000ms (5 segundos)
+        }, 200); // Delay de 500ms antes de iniciar o scroll
         event.preventDefault();
     });    
 
