d7a6b60c2ea254a4b33a3237e66f6ae5bdc44cbf
Deivid De Jesus De Moura <86139999+deividjmoura@users.noreply.github.com>
2024-04-18 17:25:54 +0000

    more

--- script/scroll.js
+++ script/scroll.js
@@ -1,21 +1,9 @@
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        setTimeout(function() {
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 10500, 'easeInOutExpo');
-        }, 500); // Delay de 500ms antes de iniciar o scroll
-        event.preventDefault();
-    });
-
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top'
-    });
-
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+$('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    setTimeout(function() {
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 5000, 'easeInOutExpo'); // Alterei o tempo de animação para 5000ms (5 segundos)
+    }, 500); // Delay de 500ms antes de iniciar o scroll
+    event.preventDefault();
 });
