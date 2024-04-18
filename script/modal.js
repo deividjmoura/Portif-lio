eae51f2273d5ddefab6f5cf5457f7b729218b4cc
Deivid De Jesus De Moura <86139999+deividjmoura@users.noreply.github.com>
2024-04-18 16:44:57 +0000

    more

--- script/modal.js
+++ script/modal.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
-    // Função para abrir o modal
-    $('.button').click(function(){
-        $('#modal-overlay').fadeIn();
+    // Função para abrir o modal quando o link "About" for clicado
+    $('#about').click(function(){
+        $('#modal-overlay').fadeIn(); // Exibe o overlay do modal
         $('#modal-content').fadeIn().css({
-            'top': '50%',
-            'left': '50%',
-            'transform': 'translate(-50%, -50%)',
-            'max-width': '50%'
+            'top': '50%', // Posiciona o modal verticalmente no centro
+            'left': '50%', // Posiciona o modal horizontalmente no centro
+            'transform': 'translate(-50%, -50%)', // Centraliza o modal
+            'max-width': '50%' // Define a largura máxima do modal
         });
-        $('body').addClass('modal-active');
+        $('body').addClass('modal-active'); // Adiciona uma classe ao corpo para evitar rolagem da página de fundo
     });
     
-    
-    // Função para fechar o modal
+    // Função para fechar o modal quando o overlay ou o botão de fechar forem clicados
     $('#modal-overlay, #modal-close').click(function(){
-        $('#modal-overlay').fadeOut();
-        $('#modal-content').fadeOut();
-        $('body').removeClass('modal-active');
+        $('#modal-overlay').fadeOut(); // Oculta o overlay do modal
+        $('#modal-content').fadeOut(); // Oculta o conteúdo do modal
+        $('body').removeClass('modal-active'); // Remove a classe do corpo para permitir a rolagem da página de fundo
     });
 });
