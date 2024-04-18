38bbc5dc8a6df515ea75e809aae0853294722616
Deivid De Jesus De Moura <86139999+deividjmoura@users.noreply.github.com>
2024-04-18 17:27:43 +0000

    more

--- script/scroll.js
+++ script/scroll.js
@@ -1,9 +1,21 @@
-$('a.page-scroll').bind('click', function(event) {
-    var $anchor = $(this);
-    setTimeout(function() {
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 5000, 'easeInOutExpo'); // Alterei o tempo de animação para 5000ms (5 segundos)
-    }, 500); // Delay de 500ms antes de iniciar o scroll
-    event.preventDefault();
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        setTimeout(function() {
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 5000, 'easeInOutExpo'); // Alterei o tempo de animação para 5000ms (5 segundos)
+        }, 500); // Delay de 500ms antes de iniciar o scroll
+        event.preventDefault();
+    });    
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top'
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
 });
