bde611a8157b828225fa883f24bd04b7bc420c7f
Deivid De Jesus De Moura <86139999+deividjmoura@users.noreply.github.com>
2024-04-18 15:55:59 +0000

    more

--- script/modal.js
+++ script/modal.js
@@ -1,18 +1,19 @@
-$('.button').click(function(){
-    var buttonId = $(this).attr('id');
-    $('#modal-container').fadeIn().removeAttr('class').addClass(buttonId + '-modal');
-    $('body').addClass('modal-active');
-  });
-  
-  $('#modal-container .close').click(function(){
-    $('#modal-container').fadeOut().addClass('out');
-    $('body').removeClass('modal-active');
-  });
-  
-  $('#modal-container').click(function(e){
-    if (e.target === this) {
-      $(this).fadeOut().addClass('out');
-      $('body').removeClass('modal-active');
-    }
-  });
-  
+$(document).ready(function() {
+    $('.button').click(function(){
+        var buttonId = $(this).attr('id');
+        $('#modal-container').fadeIn().removeAttr('class').addClass(buttonId + '-modal');
+        $('body').addClass('modal-active');
+    });
+    
+    $('#modal-container .close').click(function(){
+        $('#modal-container').fadeOut().addClass('out');
+        $('body').removeClass('modal-active');
+    });
+    
+    $('#modal-container').click(function(e){
+        if (e.target === this) {
+            $(this).fadeOut().addClass('out');
+            $('body').removeClass('modal-active');
+        }
+    });
+});
