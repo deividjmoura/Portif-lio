7ca4fa3d1c8ad2a7a704bd39687623629eab8c17
Deivid De Jesus De Moura <86139999+deividjmoura@users.noreply.github.com>
2024-04-23 14:17:44 +0000

    changes

--- script/scrollX.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const contents = document.querySelectorAll('.content');
-    contents.forEach(content => {
-      content.style.width = '100%'; // Definir a largura das divs como 100%
-    });
-  });
-  
